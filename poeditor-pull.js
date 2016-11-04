3449601ff7d5b0c8093022f733744bb97d24cb77
Sebastian Maier <sebastian@progether.com>
2016-11-04 20:04:13 +0100

    added a way to by-pass the hard-coded locals mapping

--- poeditor-pull.js
+++ poeditor-pull.js
@@ -1,34 +1,39 @@
-var fs			= require('fs'),
-	program  	= require('commander'),
-	CLI		 	= require('clui'),
-	clc			= require('cli-color'),
-	configure	= require('./configure'),
-	api			= require('./api'),
-	exporters   = require('./exporters');
+var fs = require('fs'),
+	program = require('commander'),
+	CLI = require('clui'),
+	clc = require('cli-color'),
+	configure = require('./configure'),
+	api = require('./api'),
+	exporters = require('./exporters');
 
 program.version("1.0.1")
 	   .description("Download translations from POEditor into the target directory");
 
-configure(program).then(function(config){
+configure(program).then(function (config) {
 	var files = {
 		client: {},
 		server: {},
-		base: 	{}
+		base: {}
 	};
 
+	// map locale identifier
 	var localesMap = {
-		'en-us': ['', 'en'],
-		de: ['de'],
-		no: ['nb']
-	},
-	defaultLanguage = 'en-us';
+			'en-us': ['', 'en'],
+			de: ['de'],
+			no: ['nb']
+		},
+		defaultLanguage = 'en-us';
 
 	console.log("\nPulling newest translations...\n");
 	var spinner = new CLI.Spinner('Please wait, retrieving translations for ' + config.projectLanguages[0] + '.');
 
-	var tasks = config.projectLanguages.map(function(code){
-		return function(){
-			var languages = localesMap[code];
+	var tasks = config.projectLanguages.map(function (code) {
+		return function () {
+
+			var languages = code;
+			if (localesMap.hasOwnProperty(code)) {
+				languages = localesMap[code];
+			}
 
 			spinner.message('Please wait, retrieving translations for ' + code + '.');
 			spinner.start();
@@ -36,33 +41,33 @@ configure(program).then(function(config){
 			api.apiRequest(config.apiToken, 'view_terms', {
 				id: config.projectId,
 				language: code
-			}).then(function(res){
+			}).then(function (res) {
 				var response = res[0],
-						list = res[1];
+					list = res[1];
 
 				languages.forEach(function (lang) {
-					list.forEach(function(item){
+					list.forEach(function (item) {
 						var tags = item.tags;
 
-						if(!Array.isArray(tags)){
+						if (!Array.isArray(tags)) {
 							tags = [];
 						}
 
-						if(item.context && !item.context.match(/\s/ig)){
+						if (item.context && !item.context.match(/\s/ig)) {
 							tags.push(item.context);
 						}
 
-						tags.forEach(function(file){
-							if(!files.hasOwnProperty(file)){
+						tags.forEach(function (file) {
+							if (!files.hasOwnProperty(file)) {
 								files[file] = {};
 							}
-							
-							if(!files[file].hasOwnProperty(lang)){
+
+							if (!files[file].hasOwnProperty(lang)) {
 								files[file][lang] = {};
 							}
 
 							var value = item.definition.form;
-							if(value){
+							if (value) {
 								value = value.replace(/(?:\r\n|\r|\n)/g, '\\n');
 							} else {
 								value = '';
@@ -80,27 +85,27 @@ configure(program).then(function(config){
 		}
 	});
 
-	tasks.push(function writeTranslationsToFiles(){
+	tasks.push(function writeTranslationsToFiles() {
 		spinner.message('Please wait, writing translations to files...');
 		spinner.start();
 
-		if(!config.targetDir){
+		if (!config.targetDir) {
 			config.targetDir = './';
 			console.log(clc.yellow("\n[WARNING] Target directory for translation files is not set. Using current directory as target."));
 		} else {
-			if(config.targetDir.charAt(config.targetDir.length - 1) !== '/'){
+			if (config.targetDir.charAt(config.targetDir.length - 1) !== '/') {
 				config.targetDir += '/';
 			}
 		}
 
 		try {
-			if(config.exportType.match(/[^\.]*\.js/ig)){
+			if (config.exportType.match(/[^\.]*\.js/ig)) {
 				var customExporter = require(process.cwd() + '/' + config.exportType);
-				customExporter(config, files, function(){
+				customExporter(config, files, function () {
 					spinner.stop();
 				});
-			} else if (exporters[config.exportType]){
-				exporters[config.exportType](config, files, function(){
+			} else if (exporters[config.exportType]) {
+				exporters[config.exportType](config, files, function () {
 					spinner.stop();
 				});
 			} else {
@@ -114,9 +119,9 @@ configure(program).then(function(config){
 	});
 
 	var currentTaskIndex = -1;
-	var nextTask = function(){
+	var nextTask = function () {
 		currentTaskIndex++;
-		if(currentTaskIndex >= 0 && currentTaskIndex < tasks.length){
+		if (currentTaskIndex >= 0 && currentTaskIndex < tasks.length) {
 			tasks[currentTaskIndex]();
 		}
 	};
